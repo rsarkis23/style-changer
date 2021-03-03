25e700dc1362b628ffbb0e055af79683371ce510
rsarkis23 <learning.r2@gmail.com>
2021-03-03 15:10:51 -0500

    finished project

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -6,6 +6,8 @@ export default class TextContainer extends Component {
     this.state = {
       text: '',
     };
+
+    this.updateText = this.updateText.bind(this)
   }
 
   updateText(e) {
@@ -19,6 +21,7 @@ export default class TextContainer extends Component {
       <div className="textContainer">
         <textarea
           // style={  }
+          style={ { fontFamily: this.props.fontFamily, color: this.props.fontColor, fontSize: this.props.fontSize } }
           onChange={this.updateText}
           value={this.state.text}
           placeholder="Start typing your thoughts here!"
